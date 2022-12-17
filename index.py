e4ed97a459e68e61f697fe1e632b6dd8319ef421
Rubyjia0623 <113332952+Rubyjia0623@users.noreply.github.com>
2022-12-17 14:38:37 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,6 @@
+from flask import Flask, render_template, request, make_response, jsonify
+from datetime import datetime
+
 import firebase_admin
 import random
 from firebase_admin import credentials, firestore
@@ -5,37 +8,15 @@ cred = credentials.Certificate("riddle.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-from flask import Flask, render_template, request
-from datetime import datetime
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    req = request.get_json(force=True)
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
+    return make_response(jsonify({"fulfillmentText": info}))
 
-app = Flask(__name__)
 
-@app.route("/")
-def index():
-    homepage = "<h1>猜謎語</h1>"
-    homepage += "<a href=/item>物品型謎語</a><br>"
-    homepage += "<a href=/place>地方型謎語</a><br>"
-    homepage += "<a href=/slang>俚語型謎語</a><br>"
-    homepage += "<a href=/random>隨機型謎語</a><br>"
-    return homepage
 
-@app.route("/item",methods=["GET", "POST"])
-def item():
-
-    result =""
-
-    if request.method == "POST":
-        keyword = request.form["keyword"]
-
-        collection_ref = db.collection("item")
-        docs = collection_ref.get()
-        for doc in docs:
-            dict = doc.to_dict()
-            if keyword in dict["num"]:
-                result = format(dict["Question"])
-
-        if result == "":
-            result = "是怎樣?皮啊?給我重輸"
-        return result
-    else:
-        return render_template("item.html")
+if __name__ == "__main__":
+    app.run()
