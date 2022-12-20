a4b690f242f1e6334a18966d083cb978ffb1400d
Rubyjia0623 <113332952+Rubyjia0623@users.noreply.github.com>
2022-12-20 23:26:46 +0800

    Update index.py

--- index.py
+++ index.py
@@ -8,16 +8,21 @@ cred = credentials.Certificate("riddle.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
+
 app = Flask(__name__)
 @app.route("/webhook", methods=["POST"])
 def webhook():
     req = request.get_json(force=True)
     action =  req.get("queryResult").get("action")
-    msg =  req.get("queryResult").get("queryText")
-    info = "動作：" + action + "； 查詢內容：" + msg
+    if(action == "keywordchoice"):
+        keyword = req.get("queryResult").get("parameters").get("keyword");
+        info = "您選擇的電影分級是:"+ keyword
+    #msg =  req.get("queryResult").get("queryText")
+    #info = "動作：" + action + "； 查詢內容：" + msg
     return make_response(jsonify({"fulfillmentText": info}))
 
 
 
 if __name__ == "__main__":
     app.run()
+
