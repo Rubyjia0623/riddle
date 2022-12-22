ea6e22628f44fa6d5068bc29124f9dac71836fd6
Rubyjia0623 <113332952+Rubyjia0623@users.noreply.github.com>
2022-12-22 19:03:21 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,29 +1,30 @@
 from flask import Flask, render_template, request, make_response, jsonify
-
+import random
 
 import firebase_admin
-import random
 from firebase_admin import credentials, firestore
 cred = credentials.Certificate("riddle.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-
 app = Flask(__name__)
+
 @app.route("/webhook", methods=["POST"])
 def webhook():
     req = request.get_json(force=True)
     action =  req.get("queryResult").get("action")
+    info = ""
     if(action == "keywordchoice"):
         keyword = req.get("queryResult").get("parameters").get("keyword")
         collection_ref = db.collection("aaa")
         docs = collection_ref.get()
         result = ""
-        dict = doc.to_dict()
-        result += "題目：" + dict["Question"] + "\n"
-        result += "答案：" + dict["Answer"] + "\n"
-        result += "相關資料：" + dict["Explanation"] + "\n"
-        result += "連結：" + dict["Link"] + "\n"
+        for doc in docs:
+            dict = doc.to_dict()
+            result += "題目：" + dict["Question"] + "\n"
+            result += "答案：" + dict["Answer"] + "\n"
+            result += "相關資料：" + dict["Explanation"] + "\n"
+            result += "連結：" + dict["Link"] + "\n"
         info += result
     return make_response(jsonify({"fulfillmentText": info}))
 
